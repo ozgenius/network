cdbe4eb3d24280d8a9d71fe7e3313adf4668d56f
ozgenius <ozgeniussoft@gmail.com>
2017-04-04 01:18:48 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HomePage } from '../pages/home/home';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Network } from '@ionic-native/network';
-import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
@@ -22,7 +21,6 @@ import { Geolocation } from '@ionic-native/geolocation';
     HomePage
   ],
   providers: [
-    Geolocation,
      Network,
     StatusBar,
     SplashScreen,
